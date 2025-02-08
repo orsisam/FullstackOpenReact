d613f05871b0f172c77d623b46574b9fd33e6b85
Isro Hidayatulloh <orsi.amax@gmail.com>
2025-02-08 21:23:36 +0700

    Execise 1.9 - 1.10 Unicafe

--- part1/unicafe/src/components/Statistic.jsx
+++ part1/unicafe/src/components/Statistic.jsx
@@ -3,15 +3,23 @@ export default function Statistic({ good, neutral, bad }) {
   const average = (good - bad) / total;
   const positive = (good / total) * 100;
 
-  return (
-    <div>
-      <h1>statistics</h1>
+  const displayNone = <p>No feedback given</p>;
+
+  const display = (
+    <>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {total}</p>
       <p>average {average}</p>
       <p>positive {positive} %</p>
+    </>
+  );
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      {total === 0 ? displayNone : display}
     </div>
   );
 }
