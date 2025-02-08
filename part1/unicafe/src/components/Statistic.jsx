a5f71c0663f44c87b6e73834c447b47ab623a534
Isro Hidayatulloh <orsi.amax@gmail.com>
2025-02-08 21:30:38 +0700

    Exercise 1.11 Unicafe

--- part1/unicafe/src/components/Statistic.jsx
+++ part1/unicafe/src/components/Statistic.jsx
@@ -5,21 +5,43 @@ export default function Statistic({ good, neutral, bad }) {
 
   const displayNone = <p>No feedback given</p>;
 
-  const display = (
+  const tableDisplay = (
     <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td>{total}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{average}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{positive} %</td>
+          </tr>
+        </tbody>
+      </table>
     </>
   );
 
   return (
     <div>
       <h1>statistics</h1>
-      {total === 0 ? displayNone : display}
+      {total === 0 ? displayNone : tableDisplay}
     </div>
   );
 }
